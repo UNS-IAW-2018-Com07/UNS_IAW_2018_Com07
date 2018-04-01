ae1748a8223f689ceae7ba6fa599cd44c296e261
Caterina-Panzone <caterina.p@hotmail.com>
2018-04-01 13:40:11 -0300

    Prueba logo azul

--- public_html/js/cambioEstilos/administradorEstilos.js
+++ public_html/js/cambioEstilos/administradorEstilos.js
@@ -4,11 +4,11 @@ function cambiarEstilo(value){
 
 function myMap() {
     var mapOptions = {
-        center: new google.maps.LatLng(-38.7167 -62.2833),
-        zoom: 10,
+        center: new google.maps.LatLng(-38.7167, -62.2603),
+        zoom: 12,
         mapTypeId: google.maps.MapTypeId.ROADMAP
     }
-    
+   
     //Crea un nuevo mapa dentro del elemento con identificacion "mapa" usando 
     //los parametros dados en mapOptions. 
     var map = new google.maps.Map(document.getElementById("mapa"), mapOptions);
