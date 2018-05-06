369814db7c95aa8d81d6e1f3b183df25fa9844cf
Stephanie Malvicini <s.a.m_puan@hotmail.com>
2018-05-06 19:24:30 -0300

    probando inicio sesion

--- app_auth/config.js
+++ app_auth/config.js
@@ -3,7 +3,8 @@ var config = {
     'googleAuth' : {
         'clientID'         : '295229625606-n9qt2kign264nocnmhl9c6ujnd2cr7jh.apps.googleusercontent.com',
         'clientSecret'     : '4KH0pp0dst9WyZm7e8mbjCJ7',
-        'callbackURL'      : 'http://localhost:3000/auth/google/callback'
+         'callbackURL'      : 'https://ktphi.herokuapp.com/auth/google/callback'
+        // 'callbackURL'      : 'http://localhost:3000/auth/google/callback'
     }
     
 };
