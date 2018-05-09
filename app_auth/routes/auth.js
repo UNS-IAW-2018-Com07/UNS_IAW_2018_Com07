5d2b6a8b7e22244a31fe94767d559bc4b687181d
Caterina-Panzone <caterina.p@hotmail.com>
2018-05-08 23:48:11 -0300

    cambios menores

--- app_auth/routes/auth.js
+++ app_auth/routes/auth.js
@@ -2,12 +2,11 @@ var express = require('express');
 var router = express.Router();
 var passport = require('passport');
 
-var middleware = require('../auth/middleware'); 
-
 var ctrlAuthentication = require('../controllers/auth'); 
 
 // Ruta para autenticarse con Google (enlace de login)
 router.get('/google', passport.authenticate('google', { scope : 'profile' }));
+
 // Ruta de callback, a la que redirigirá tras autenticarse con Google.
 // En caso de fallo redirige a la vista '/' (inicio)
 router.get('/google/callback', 
